8d6c31e0082a028728935524a900d51727a8ddd7
Devin Huang <devin.huang@oracle.com>
2025-03-25 10:42:17 +1100

    fix joindate

--- src/main/java/com/example/user_management/config/RedisConfig.java
+++ src/main/java/com/example/user_management/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.example.user_management.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,6 +23,9 @@ public class RedisConfig {
         // 创建 Jackson 序列化器，并注册 JavaTimeModule 以支持 LocalDate
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());  // 解决 LocalDate 无法序列化的问题
+
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);  // ✅ 禁止序列化为 [year, month, day]
+
         GenericJackson2JsonRedisSerializer serializer = new GenericJackson2JsonRedisSerializer(objectMapper);
 
         template.setValueSerializer(serializer);
